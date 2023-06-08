306f6c0e48b20524c1281ba20d481aad594b4e3d
Manas0410 <manasshrivastava0410@gmail.com>
2023-06-08 07:43:05 -0700

    new

--- src/FunctionComp/FormAll.jsx
+++ src/FunctionComp/FormAll.jsx
@@ -22,13 +22,13 @@ const FormAll = () => {
   };
   return (
     <div>
-      <p>{profile.name}</p>
+      <p>name: {profile.name}</p>
       <input type="text" name="name" onChange={myChange} />
-      <p>{profile.email}</p>
+      <p>mail:{profile.email}</p>
       <input type="text" name="email" onChange={myChange} />
-      <p>{profile.passwaord}</p>
+      <p>pass:{profile.passwaord}</p>
       <input type="password" name="pass" onChange={myChange} />
-      <p>{profile.city}</p>
+      <p>city:{profile.city}</p>
       <label for="cars">Choose a car:</label>
       <select name="city" onChange={myChange}>
         <option>UP</option>
@@ -36,19 +36,19 @@ const FormAll = () => {
         <option>Mumbai</option>
         <option>Delhi</option>
       </select>
-      <p>{profile.gender}</p>
+      <p>gender:{profile.gender}</p>
       <input type="radio" name="gender" value="male" onChange={myChange} />
       <label for="male">male</label>
       <input type="radio" name="gender" value="female" onChange={myChange} />
       <label for="male">female</label>
-      <p>{profile.hobby}</p>
-      <input type="checkbox" name="hobby" value="Bike" onChange={myChange} />
+      <p>hobby:{profile.hobby}</p>
+      <input type="checkbox" name="hobby" value="cricket" onChange={myChange} />
       <label for="vehicle1"> cricket</label>
       <input type="checkbox" name="hobby" value="Car" onChange={myChange} />
       <label for="vehicle2"> car</label>
-      <input type="checkbox" name="hobby" value="Boat" onChange={myChange} />
+      <input type="checkbox" name="hobby" value="bike" onChange={myChange} />
       <label for="vehicle3"> bike</label>
-      <p>{profile.rating}</p>
+      <p>rating:{profile.rating}</p>
       <input
         type="range"
         min="1"
