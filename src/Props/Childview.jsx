21dd4f8c84cc64787b68ebdab7152a3674417c81
Manas0410 <manasshrivastava0410@gmail.com>
2023-06-13 07:52:19 -0700

    props

---
+++ src/Props/Childview.jsx
@@ -0,0 +1,22 @@
+// const ChildView = ({name, city, number="NA"}) => { // 2nd way to handle def val
+const ChildView = ({ name, city, number }) => {
+  // console.log(meraProps);
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        height: 140,
+        width: 100,
+        padding: 12,
+        boxShadow: "silver 0px 0px 1px 1px",
+      }}
+    >
+      <p>
+        {/* {meraProps.name} from {meraProps.city} with {meraProps.number || "NA"} */}{" "}
+        {name} from {city} with {number}
+      </p>
+    </div>
+  );
+};
+ChildView.defaultProps = { number: "NA" }; // third way to handle default val.
+export default ChildView;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@
 import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
 import "./index.css";
-import Counter from "./FunctionComp/newCounter.jsx";
+import ParentView from "./Props/Parentview.jsx";
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
 // )
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Counter />);
+ReactDOM.createRoot(document.getElementById("root")).render(<ParentView />);
