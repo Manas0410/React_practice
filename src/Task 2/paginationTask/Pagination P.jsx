59cc45d109c28fac90b5fe11bdcf7f4221501688
Manas0410 <manasshrivastava0410@gmail.com>
2023-07-16 14:54:00 -0700

    pagination

---
+++ src/Task 2/paginationTask/Pagination P.jsx
@@ -0,0 +1,21 @@
+// TASK-Pagination with useEffect and useMemo
+// Create a component that fetches a list of items from an API endpoint
+//  and implements pagination. Use useEffect to fetch the initial data
+//  and subsequent data when the page changes.
+//Use useMemo to calculate the number of pages based on the total number of items
+//and items per page
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const [apiData, setApiData] = useState([]);
+const PaginationP = () => {
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => setApiData(response.data))
+      .catch((error) => console.log(error));
+  }, [apiData]);
+  return <div></div>;
+};
+
+export default PaginationP;
