51778dc629b2914898dc645088960c1e00f99edf
Manas0410 <manasshrivastava0410@gmail.com>
2023-08-16 22:09:45 -0700

    I did question of converting true to available and reverse for false

---
+++ src/utils/avail.js
@@ -0,0 +1,50 @@
+const p = [
+  {
+    id: 1,
+    name: "John Doe",
+    city: "New York",
+    state: "New York",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    city: "Los Angeles",
+    state: "California",
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: "Michael Johnson",
+    city: "Chicago",
+    state: "Illinois",
+    isActive: true,
+  },
+  {
+    id: 4,
+    name: "Emily Brown",
+    city: "Houston",
+    state: "Texas",
+    isActive: true,
+  },
+];
+
+const m = p.reduce((ac, val) => {
+  let temp = { ...val };
+  temp.isActive = val.isActive ? "available" : "unavailable";
+  ac.push(temp)
+  // ac = [...ac,temp]
+  return ac;
+}, []);
+console.log(m);
+// const temp =(arr)=>{
+//   return arr.push(0)
+// }
+// console.log(temp([1,2,3]))
+var a=1;
+a=2;
+function a(){
+console.log("hii");
+}
+a();
+console.log(a);
--- src/utils/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const result = temp.map((tempData)=>{
-// const res = arr.filter((arData)=>arData.val !==tempData)
-// arr = [... res]
-// })
