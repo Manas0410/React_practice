1d4c65f1825f36dfc103beabaa7578b6e0b6118d
manas0410 <manasshrivastava0410@gmail.com>
2024-01-10 04:22:43 -0800

    services comp

---
+++ src/msxcomps/services/Services.jsx
@@ -0,0 +1,16 @@
+const ServicesComp = () => {
+  return (
+    <div>
+      <p>We provide our services in 3 categories</p>
+      <div className="service-card">
+        <p>Portfolio Website</p>
+      </div>
+      <div className="service-card">
+        <p>Static E-commerce Website</p>
+      </div>
+      <div className="service-card">
+        <p>Online E-commerce Store</p>
+      </div>
+    </div>
+  );
+};
